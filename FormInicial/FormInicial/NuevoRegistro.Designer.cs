1eea2f19d536a59af5b6e967e992a24008c1ddfc
Alejandro Armenta <marioarmenta@outlook.com>
2016-12-08 22:37:20 -0700

    Form Huella Digital Add

--- FormInicial/FormInicial/NuevoRegistro.Designer.cs
+++ FormInicial/FormInicial/NuevoRegistro.Designer.cs
@@ -34,12 +34,18 @@
             this.txtApp = new System.Windows.Forms.TextBox();
             this.txtApm = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 13);
+            this.label1.Location = new System.Drawing.Point(25, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 0;
@@ -48,15 +54,15 @@
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(146, 10);
+            this.txtNombre.Location = new System.Drawing.Point(145, 29);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(372, 20);
+            this.txtNombre.Size = new System.Drawing.Size(349, 20);
             this.txtNombre.TabIndex = 1;
             // 
             // lblApp
             // 
             this.lblApp.AutoSize = true;
-            this.lblApp.Location = new System.Drawing.Point(26, 41);
+            this.lblApp.Location = new System.Drawing.Point(25, 60);
             this.lblApp.Name = "lblApp";
             this.lblApp.Size = new System.Drawing.Size(114, 13);
             this.lblApp.TabIndex = 2;
@@ -64,32 +70,84 @@
             // 
             // txtApp
             // 
-            this.txtApp.Location = new System.Drawing.Point(147, 41);
+            this.txtApp.Location = new System.Drawing.Point(146, 60);
             this.txtApp.Name = "txtApp";
-            this.txtApp.Size = new System.Drawing.Size(371, 20);
+            this.txtApp.Size = new System.Drawing.Size(348, 20);
             this.txtApp.TabIndex = 3;
             // 
             // txtApm
             // 
-            this.txtApm.Location = new System.Drawing.Point(147, 68);
+            this.txtApm.Location = new System.Drawing.Point(146, 87);
             this.txtApm.Name = "txtApm";
-            this.txtApm.Size = new System.Drawing.Size(371, 20);
+            this.txtApm.Size = new System.Drawing.Size(348, 20);
             this.txtApm.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 74);
+            this.label2.Location = new System.Drawing.Point(25, 90);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(116, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "APELLIDO MATERNO";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(262, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Apodo";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(28, 126);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 43);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Genero";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 16);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(88, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "MASCULINO";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(97, 16);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(81, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "FEMENINO";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(317, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 20);
+            this.textBox1.TabIndex = 9;
+            // 
             // NuevoRegistro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(613, 262);
+            this.ClientSize = new System.Drawing.Size(561, 262);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtApm);
             this.Controls.Add(this.txtApp);
@@ -98,6 +156,8 @@
             this.Controls.Add(this.label1);
             this.Name = "NuevoRegistro";
             this.Text = "NuevoRegistro";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,5 +171,10 @@
         private System.Windows.Forms.TextBox txtApp;
         private System.Windows.Forms.TextBox txtApm;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
