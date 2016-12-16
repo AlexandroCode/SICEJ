16a6d47951a75349e09fefba9c1c48a6f29ece24
jorge <jorge_eduardo1995@hotmail.com>
2016-12-16 15:10:54 -0700

    Se actualizo la clase ReclusosData

--- FormInicial/FormInicial/ReclusosData.cs
+++ FormInicial/FormInicial/ReclusosData.cs
@@ -11,11 +11,11 @@ namespace FormInicial
     {
         public static int Agregar(Recluso aRecluso)
         {
-            int retorno = 0;
+            int ret = 0;
             MySqlCommand comando = new MySqlCommand(string.Format("Insert into Recluso (Nombre, App, Apm, Rfc, Apodo, Genero, Lugarnacimiento, Fechanacimiento, EstadoCivil, ID_escolaridad, Ocupacion, GrupoEtnico, Conyuge, NomPat, NomMat, Telefono, Parentezco, ID_nacionalidad) values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11},'{12}','{13}','{14},'{15}','{16}','{17}')",
                 aRecluso.Nombre, aRecluso.App, aRecluso.Apm, aRecluso.rfc, aRecluso.Apodo, aRecluso.Genero, aRecluso.Lugarnacimiento, aRecluso.Fechanacimiento, aRecluso.estadocivil, aRecluso.id_escolaridad, aRecluso.ocupacion, aRecluso.grupoetnico, aRecluso.conyuge, aRecluso.nombrepadre, aRecluso.nombremadre, aRecluso.telefono, aRecluso.parentesco, aRecluso.id_nacionalidad),ConectorBD.ObtenerConexion());
-            retorno = comando.ExecuteNonQuery();
-            return retorno;
+            ret = comando.ExecuteNonQuery();
+            return ret;
         }
     }
 }
